cec7d9e9c99fdace03752bc4e728bfa7f66db07d
julietafc <juli47g1@stud.kea.dk>
2021-08-30 15:16:37 +0200

    move functions

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
 "use strict";
 
-const operator = ["+", "-", "*", "/"];
+let firstNumber;
+let secondNumber;
+let result;
+
 const round = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 start();
 
 function start() {
   console.log("start");
-  //read first number
-  let firstNumber = document.getElementById("firstnumber").value;
-  //read second number
-  let secondNumber = document.getElementById("secondnumber").value;
+
   //read operator
-  let result = document.getElementById("results").value;
-  if (operator == "+") {
-    console.log("This is a sum");
-    result = firstNumber + secondNumber;
-  } else if (operator == "-") {
-    console.log("This is a rest");
-    result = firstNumber - secondNumber;
-  } else if (operator == "*") {
-    console.log("This is a multiplication");
-    result = irstNumber * secondNumber;
-  } else {
-    console.log("This is a divition");
-    result = firstNumber / secondNumber;
-  }
   document.getElementById("calculate").addEventListener("click", showResult);
 }
 
 function showResult() {
   console.log("showResult");
   document.getElementById("clear").addEventListener("click", clearResults);
-  // round up number if needed
+  let operator = document.getElementById("operator").value;
+
+  //read first number
+  firstNumber = document.getElementById("firstnumber").value;
+  //read second number
+  secondNumber = document.getElementById("secondnumber").value;
+
+  if (operator == "add") {
+    console.log("This is a sum");
+    result = firstNumber + secondNumber;
+  } else if (operator == "sub") {
+    console.log("This is a rest");
+    result = firstNumber - secondNumber;
+  } else if (operator == "mul") {
+    console.log("This is a multiplication");
+    result = firstNumber * secondNumber;
+  } else {
+    console.log("This is a divition");
+    result = firstNumber / secondNumber;
+  }
+
+  console.log("the result is "); // round up number if needed
+
   // append result in list
 }
 
