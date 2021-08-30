356660cb4066c68b3bde4f835c6320299f97ee7e
julietafc <juli47g1@stud.kea.dk>
2021-08-30 21:45:50 +0200

    adding round function halfway

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 "use strict";
 
+window.addEventListener("load", start);
+
 let firstNumber;
 let secondNumber;
 let result;
 
 const round = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-start();
-
 function start() {
   console.log("start");
-
-  //read operator
   document.getElementById("calculate").addEventListener("click", showResult);
+  document.getElementById("clear").addEventListener("click", clearResults);
 }
 
 function showResult() {
   console.log("showResult");
-  document.getElementById("clear").addEventListener("click", clearResults);
+
   let operator = document.getElementById("operator").value;
 
-  //read first number
   firstNumber = document.getElementById("firstnumber").value;
   //read second number
   secondNumber = document.getElementById("secondnumber").value;
@@ -28,18 +26,22 @@ function showResult() {
   if (operator == "add") {
     console.log("This is a sum");
     result = firstNumber + secondNumber;
+    roundResult();
   } else if (operator == "sub") {
     console.log("This is a rest");
     result = firstNumber - secondNumber;
+    roundResult();
   } else if (operator == "mul") {
     console.log("This is a multiplication");
     result = firstNumber * secondNumber;
+    roundResult();
   } else {
     console.log("This is a divition");
     result = firstNumber / secondNumber;
+    roundResult();
   }
 
-  console.log("the result is "); // round up number if needed
+  console.log(result); // round up number if needed
 
   // append result in list
 }
